9d71552cf771336f7a9ccfc8953467f34607afbf
seymseym <seym.kurt97@gmail.com>
2022-01-13 13:39:02 +0300

    Added bindings and some new functionalites

---
+++ WeatheApp/ViewModel/SearchCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WeatheApp.ViewModel
+{
+    public class SearchCommand : ICommand
+    {
+        public WeatherVM VM { get; set; }
+        public event EventHandler CanExecuteChanged;
+
+        public SearchCommand(WeatherVM vm)
+        {
+            VM = vm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            VM.makeQuery();
+        }
+    }
+}
